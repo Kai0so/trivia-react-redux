5076cca919ed379735df64d93e11dcd32af7151f
Gero <gersonhoa@gmail.com>
2022-02-15 00:43:54 -0300

    feat : acessa o token da api e adiciona funcionalidade ao botão

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Config from './pages/Config';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/config" component={ Config } />
       </Switch>
     </div>
   );
