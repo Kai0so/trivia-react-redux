baac0a8272c610ec94556054cad703f3069bec28
Gero <gersonhoa@gmail.com>
2022-02-15 01:27:01 -0300

    fix : muda as rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
 import Config from './pages/Config';
+import Login from './pages/Login';
 
 export default function App() {
   return (
