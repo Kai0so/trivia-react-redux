71c0212db0f15a2365c0f2cf0e530a53b07610e6
Gero <gersonhoa@gmail.com>
2022-02-16 23:34:25 -0300

    fix :  troca o push por spreed

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -22,16 +22,22 @@ export default class Answers extends Component {
 
   shuffledAnswers = () => {
     const { result, index } = this.props;
-    const allAnswers = result.results[index].incorrect_answers;
-    allAnswers.push(result.results[index].correct_answer);
-
-    console.log(allAnswers);
+    let allAnswers = result.results[index].incorrect_answers;
+    allAnswers = [...allAnswers, result.results[index].correct_answer];
 
     const shuffled = allAnswers
       .map((value) => ({ value, sort: Math.random() }))
       .sort((a, b) => a.sort - b.sort)
       .map(({ value }) => value);
 
+    this.setState({
+      sortedPositions: [],
+    }, () => this.updateState(shuffled));
+  }
+
+  updateState = (shuffled) => {
+    const { result, index } = this.props;
+    console.log(shuffled);
     this.setState({
       sortedPositions: shuffled,
       correctAnswer: result.results[index].correct_answer,
@@ -40,6 +46,7 @@ export default class Answers extends Component {
 
   render() {
     const { sortedPositions, correctAnswer, isDisabled } = this.state;
+    console.log(sortedPositions);
     return (
       <div>
         <div data-testid="answer-options" className="answers_game">
@@ -79,3 +86,6 @@ export default class Answers extends Component {
 Answers.propTypes = {
   addToken: PropTypes.func,
 }.isRequired;
+
+// clear Interval
+//
